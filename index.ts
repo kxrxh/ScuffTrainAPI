ee62ce518980c6a0ae84981b3b7da311e88e5f55
isofinly <isofinly@icloud.com>
2023-11-10 18:33:36 +0300

    Завезли схему

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import server from "bunrest";
+
+/**
+ * Starts the server and listens on the specified port. 
+ */
+function main(): void {
+    const port = process.env.PORT || 3000;
+
+    const app = server();
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: req.query });
+    });
+
+    app.listen(port, () => console.log("Listening on port:", port));
+}
+
+main();
